9fadc6ad2363658bd83624ccb80fc992b4c9c985
Saqlain-Mohammed <saqlain.patel.md@gmail.com>
2026-01-31 14:48:47 -0600

    Time: 5 ms (45.13%), Space: 14.9 MB (19.02%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,42 +2,45 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
 
-        int n = nums.size();
+        //O(n) solution
 
-        for(int i=0;i<n;i++){
+        int n=nums.size();
+        unordered_map<int,int> umap;
 
-            int x = nums.at(i);
-
-            for(int j=i+1;j<n;j++){
-
-                int y = nums.at(j);
-
-                if((x+y)==target){
-                    vector<int> result;
-                    result.push_back(i);
-                    result.push_back(j);
-                    return result;
-                    break;
-                }
+        for(int i=0;i<n;i++)
+        {
+            if(umap.find(target-nums[i])!=umap.end())
+            {
+                return vector<int>{umap[target-nums[i]],i};
             }
-
+            umap[nums[i]]=i;
         }
-
+        
         return {};
 
-        
-        // int n=nums.size();
-        // unordered_map<int,int> umap;
+        //O(n2) solution
 
-        // for(int i=0;i<n;i++)
-        // {
-        //     if(umap.find(target-nums[i])!=umap.end())
-        //     {
-        //         return vector<int>{umap[target-nums[i]],i};
+        // int n = nums.size();
+
+        // for(int i=0;i<n;i++){
+
+        //     int x = nums.at(i);
+
+        //     for(int j=i+1;j<n;j++){
+
+        //         int y = nums.at(j);
+
+        //         if((x+y)==target){
+        //             vector<int> result;
+        //             result.push_back(i);
+        //             result.push_back(j);
+        //             return result;
+        //             break;
+        //         }
         //     }
-        //     umap[nums[i]]=i;
+
         // }
-        
+
         // return {};
     }
 };
