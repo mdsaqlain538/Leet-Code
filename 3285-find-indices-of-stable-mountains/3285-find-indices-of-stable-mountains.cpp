ed008835e67f39d2ad50d911763de6f12d771e1d
Saqlain-Mohammed <saqlain.patel.md@gmail.com>
2026-02-14 18:06:13 -0600

    Time: 0 ms (100%), Space: 28.2 MB (90.94%) - LeetHub

---
+++ 3285-find-indices-of-stable-mountains/3285-find-indices-of-stable-mountains.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> stableMountains(vector<int>& height, int threshold) {
+        
+        vector<int> result;
+
+        for(int i=1;i<height.size();i++){
+            if(height.at(i-1)>threshold){
+                result.push_back(i);
+            }
+        }
+        
+        return result;
+    }
+};
