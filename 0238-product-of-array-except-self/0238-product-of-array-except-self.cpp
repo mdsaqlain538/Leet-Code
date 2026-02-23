01c5bcfaf0b74dd54fb58c975f793d82ba352a48
Saqlain-Mohammed <saqlain.patel.md@gmail.com>
2026-02-23 06:29:57 -0600

    Time: 5 ms (8.86%), Space: 46.9 MB (5.33%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -5,28 +5,28 @@ public:
         vector<int> left;
         vector<int> right;
 
-        int mul = 1;
+        int left_mul = 1;
 
         left.push_back(1);
 
         for(int i=0;i<nums.size()-1;i++){
 
-            mul = mul * nums.at(i);
+            left_mul = left_mul * nums.at(i);
 
-            left.push_back(mul);
+            left.push_back(left_mul);
         }
 
         reverse(nums.begin(),nums.end());
 
         right.push_back(1);
 
-        int mal = 1;
+        int right_mul = 1;
 
         for(int i=0;i<nums.size()-1;i++){
 
-            mal = mal * nums.at(i);
+            right_mul = right_mul * nums.at(i);
 
-            right.push_back(mal);
+            right.push_back(right_mul);
         }
 
         reverse(right.begin(),right.end());
