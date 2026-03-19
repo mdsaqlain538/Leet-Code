e390b4a9aa7a0774a6be2a493eaa22188ca7e529
Saqlain-Mohammed <saqlain.patel.md@gmail.com>
2026-03-18 21:41:29 -0500

    Time: 0 ms (100%), Space: 12.8 MB (47.61%) - LeetHub

---
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        
+        sort(arr.begin(),arr.end());
+
+        int k = arr[1]-arr[0], n = arr.size(), count = 0;
+
+        for(int i=0;i<n-1;i++){
+
+            if(arr[i+1]-arr[i]==k){
+                count++;
+            }
+        }
+
+        if(count==n-1){
+            return true;
+        }
+
+        return false;
+    }
+};
