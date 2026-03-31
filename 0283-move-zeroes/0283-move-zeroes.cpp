a4dbdd0102e3cffc4b63ec0086c2ff0b79e556e6
Saqlain-Mohammed <saqlain.patel.md@gmail.com>
2026-03-30 20:44:16 -0500

    Time: 488 ms (5.03%), Space: 23.9 MB (54.96%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+
+        for(int i=0;i<nums.size();i++){
+
+            for(int j=i+1;j<nums.size();j++){
+
+                if(nums.at(i)==0 && nums.at(j)!=0){
+                    swap(nums.at(i),nums.at(j));
+                    break;
+                }
+            }
+        }
+
+    }
+};
